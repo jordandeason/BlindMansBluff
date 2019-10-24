9e96719e163c2d84bbf4af5fb79745a3d73eaa78
JordanDeason <deasjr17@wfu.edu>
2019-10-24 14:19:53 -0400

    Almost finished

---
+++ cmake-build-debug/CMakeFiles/BlindMansBluff.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/JordanDeason/CLionProjects/project5-jordandeason/Card.cpp" "/Users/JordanDeason/CLionProjects/project5-jordandeason/cmake-build-debug/CMakeFiles/BlindMansBluff.dir/Card.cpp.o"
+  "/Users/JordanDeason/CLionProjects/project5-jordandeason/Deck.cpp" "/Users/JordanDeason/CLionProjects/project5-jordandeason/cmake-build-debug/CMakeFiles/BlindMansBluff.dir/Deck.cpp.o"
+  "/Users/JordanDeason/CLionProjects/project5-jordandeason/main.cpp" "/Users/JordanDeason/CLionProjects/project5-jordandeason/cmake-build-debug/CMakeFiles/BlindMansBluff.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/BlindMansBluff.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BlindMansBluff"
+  "BlindMansBluff.pdb"
+  "CMakeFiles/BlindMansBluff.dir/Card.cpp.o"
+  "CMakeFiles/BlindMansBluff.dir/Deck.cpp.o"
+  "CMakeFiles/BlindMansBluff.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BlindMansBluff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
